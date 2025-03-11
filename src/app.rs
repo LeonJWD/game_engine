4c8283563d7f144fdca7e0f2f922a3c777c28c17
LeonJWD <131593543+LeonJWD@users.noreply.github.com>
2025-03-11 16:46:02 +0100

    basic window

---
+++ src/app.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+
+use crate::app_state::State;
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[derive(Default)]
+pub struct App {
+    state: Option<State>,
+}
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+        let state = pollster::block_on(State::new(window.clone()));
+
+        self.state = Some(state);
+
+        window.request_redraw();
+    }
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        let state = self.state.as_mut().unwrap();
+
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed. stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                state.render();
+                state.get_window().request_redraw();
+            }
+            WindowEvent::Resized(size) => {
+                state.resize(size);
+            }
+            _ => (),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::sync::Arc;
+
+use app::App;
+use app_state::State;
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+mod app;
+mod app_state;
+
+fn main() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
